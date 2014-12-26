617a8259cceba8f1ebd56efe03277b128f8bb0d0
perlancar <perlancar@gmail.com>
2014-12-26 17:01:22 +0000

    Import of PERLANCAR/App-CompleteUtils-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: App-CompleteUtils
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/App-CompleteUtils-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

---
+++ lib/App/CompleteUtils.pm
@@ -0,0 +1,66 @@
+package App::CompleteUtils;
+
+our $DATE = '2014-12-27'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Collection of CLI utilities for Dist::Zilla
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::CompleteUtils - Collection of CLI utilities for Dist::Zilla
+
+=head1 VERSION
+
+This document describes version 0.01 of App::CompleteUtils (from Perl distribution App-CompleteUtils), released on 2014-12-27.
+
+=head1 SYNOPSIS
+
+This distribution provides several command-line utilities related to
+(shell) completion and the L<Complete> modules family.
+
+=head1 SEE ALSO
+
+L<Complete>
+
+Frameworks you can test this with: L<Perinci::CmdLine>,
+L<Getopt::Long::Complete>, L<Getopt::Long::Subcommand>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-CompleteUtils>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-App-CompleteUtils>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-CompleteUtils>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
